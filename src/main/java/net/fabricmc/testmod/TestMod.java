dd9d5737f517d416341e1604350670b05157d496
SingleSimha <70630469+SingleSimha@users.noreply.github.com>
2022-05-21 17:17:36 +0530

    Added dildo statue

--- src/main/java/net/fabricmc/testmod/TestMod.java
+++ src/main/java/net/fabricmc/testmod/TestMod.java
@@ -2,7 +2,12 @@ package net.fabricmc.testmod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.testmod.blocks.DildoStatue;
 import net.fabricmc.testmod.items.DildoSword;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.SoundEvent;
@@ -17,10 +22,14 @@ public class TestMod implements ModInitializer {
 	public static final Item DILDO_SWORD = new DildoSword(new FabricItemSettings().group(ItemGroup.MISC));
 	public static final Identifier GOAT_SCREAM_ID = new Identifier("testmod","goat_scream");
 	public static final SoundEvent GOAT_SCREAM_SOUND = new SoundEvent(GOAT_SCREAM_ID);
+
+	public static final Block DILDO_STATUE = new DildoStatue(FabricBlockSettings.of(Material.METAL).strength(4.0f));
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 		Registry.register(Registry.ITEM,new Identifier("testmod","dildo_sword"),DILDO_SWORD);
 		Registry.register(Registry.SOUND_EVENT,GOAT_SCREAM_ID,GOAT_SCREAM_SOUND);
+		Registry.register(Registry.BLOCK, new Identifier("testmod", "dildo_statue"), DILDO_STATUE);
+		Registry.register(Registry.ITEM, new Identifier("testmod", "dildo_statue"), new BlockItem(DILDO_STATUE, new FabricItemSettings().group(ItemGroup.MISC)));
 	}
 }
